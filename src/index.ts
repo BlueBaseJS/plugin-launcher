bab899043bef6b905a9d008be196b6a0f5e08a78
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-08-28 05:49:24 +0500

    feat: Plugin now uses dynamic version

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { DefaultIcon, LauncherScreen } from './components';
+
+import { VERSION } from './version';
 import { createPlugin } from '@bluebase/core';
 
 export default createPlugin({
 	description: 'A plugin that replaces HomeScreen to show Apps just like any mobile OS',
 	key: 'plugin-launcher',
 	name: 'BlueBase App Launcher',
-	version: '1.0.0',
+	version: VERSION,
 
 	components: {
 		HomeScreen: LauncherScreen,
