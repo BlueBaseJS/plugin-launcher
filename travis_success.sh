3378d8c008a82869ccb44f42bedf45c5b3d52677
Muhammad Danial Iqbal <muhammaddanialiqbal@Muhammads-MacBook-Pro-3.local>
2019-09-02 21:25:49 +0500

    fix(Now): Added curl in yml file

--- travis_success.sh
+++ travis_success.sh
@@ -1,4 +1,1 @@
-cd build/web/client/
-URL="$(now --token=${NW_TOKEN})"
-cd ../../../
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"Build Path: ${URL}\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
