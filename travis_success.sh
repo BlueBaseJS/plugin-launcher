23f8ec953950942dc05c9aa6494ac5941d07d170
Muhammad Danial Iqbal <muhammaddanialiqbal@Muhammads-MacBook-Pro-3.local>
2019-09-02 19:07:13 +0500

    fix(Now): Added build path in script

--- travis_success.sh
+++ travis_success.sh
@@ -1,7 +1,1 @@
-curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
--d "{\"body\": echo \"${URL}\"}" \
-"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
-echo "${TRAVIS_REPO_SLUG}"
-echo "${TRAVIS_PULL_REQUEST}"
-echo "${GITHUB_TOKEN}"
-echo "${URL}"
+curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"Build Path: ${URL}\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
