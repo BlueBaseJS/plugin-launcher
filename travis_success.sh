717081509d71d0e1167740e72e6ec6901e324ac1
Muhammad Danial Iqbal <muhammaddanialiqbal@Muhammads-MacBook-Pro-3.local>
2019-09-02 17:47:11 +0500

    fix(Now Post Script): Testing success script

--- travis_success.sh
+++ travis_success.sh
@@ -1,3 +1,1 @@
-curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
--d "{\"body\": \"Hello world\"}" \
-"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \ -d "{\"body\": \"Hello world\"}" \ "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
