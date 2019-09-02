29f7dcd49b41d6128ba826ea6c25175e0554a250
Muhammad Danial Iqbal <muhammaddanialiqbal@Muhammads-MacBook-Pro-3.local>
2019-09-02 18:43:38 +0500

    feat(Now): working on comment shell script

--- travis_success.sh
+++ travis_success.sh
@@ -1,3 +1,7 @@
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
 -d "{\"body\": echo \"${URL}\"}" \
 "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+echo "${TRAVIS_REPO_SLUG}"
+echo "${TRAVIS_PULL_REQUEST}"
+echo "${GITHUB_TOKEN}"
+echo "${URL}"
