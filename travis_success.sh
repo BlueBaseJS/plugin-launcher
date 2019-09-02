768f3a10612aed6ed340178dda1a45b332344b55
Muhammad Danial Iqbal <muhammaddanialiqbal@Muhammads-MacBook-Pro-3.local>
2019-09-02 21:10:44 +0500

    fix(Now): Moved now command in shell

--- travis_success.sh
+++ travis_success.sh
@@ -1,1 +1,4 @@
+cd build/web/client/
+URL="$(now --token=${NW_TOKEN})"
+cd ../../../
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"Build Path: ${URL}\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
