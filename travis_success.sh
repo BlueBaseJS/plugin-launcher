dfce004de9f8229436f7b0cc22b696e5d35a773c
Muhammad Danial Iqbal <muhammaddanialiqbal@Muhammads-MacBook-Pro-3.local>
2019-09-03 14:48:24 +0500

    fix(Now): Working on sh file t curl

--- travis_success.sh
+++ travis_success.sh
@@ -1,1 +1,2 @@
+URL="$(now --token=${NW_TOKEN})"
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"Build Path: ${URL}\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
