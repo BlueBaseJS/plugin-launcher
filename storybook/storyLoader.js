62ba8f21fa60b209c00fddf616a7aadafc5bceda
Abdul Rehman Talat <rehman.talat@gmail.com>
2023-09-09 16:33:36 +0500

    feat: Expo 49

---
+++ storybook/storyLoader.js
@@ -0,0 +1,19 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+	require('../src/components/AppGrid/__stories__/AppGrid.stories');
+	require('../src/components/LauncherScreen/__stories__/LauncherScreen.stories');
+}
+
+const stories = [
+	'../src/components/AppGrid/__stories__/AppGrid.stories',
+	'../src/components/LauncherScreen/__stories__/LauncherScreen.stories',
+];
+
+module.exports = {
+	loadStories,
+	stories,
+};
